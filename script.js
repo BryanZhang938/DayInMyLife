ee20ec5bc7a38703243d280cc2cb59ca02781b4a
Turki Alrasheed <turkial2005@gmail.com>
2025-05-29 16:05:34 -0700

    some stuff

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 import { parseActivity, drawActivity } from './activity.js';
 import { parseHeartRate, drawHeartRate } from './heartrate.js';
 
+const params = new URLSearchParams(window.location.search);
+const selectedUser = params.get("user");
 // Load and process data
 Promise.all([
-  d3.csv("cleaned_data/all_actigraph.csv", parseActivity),
-  d3.csv("cleaned_data/all_actigraph.csv", parseHeartRate)
+  d3.csv("../cleaned_data/all_actigraph.csv", parseActivity),
+  d3.csv("../cleaned_data/all_actigraph.csv", parseHeartRate)
 ]).then(([activityData, heartRateData]) => {
-  drawActivity(activityData);
-  drawHeartRate(heartRateData);
-}).catch(error => {
-  console.error("Error loading data:", error);
-}); 
+  if (selectedUser) {
+    const filteredActivity = activityData.filter(d => d.user === selectedUser);
+    const filteredHeartRate = heartRateData.filter(d => d.user === selectedUser);
+
+    drawActivity(filteredActivity);
+    drawHeartRate(filteredHeartRate);
+  } else {
+    console.warn("No user selected in URL (use ?user=user_1)");
+  }
+});
